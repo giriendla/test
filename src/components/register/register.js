29db926c9cc7ebd9403b437514c8f20ac77b0633
Sai-siri <saisiri.k@techvedika.com>
2018-09-11 11:44:10 +0530

    UI design registration page - Done

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
-import { Button,Grid, Menu, MenuItem } from '@material-ui/core';
-import { Scrollbars } from 'react-custom-scrollbars';
-import MainNav from '../_/navigation';
+import axios from 'axios';
+import { Grid, TextField } from '@material-ui/core';
 import Config from '../../container/config';
+import {getEmployees} from '../../service/api';
 
-export default class Register extends Component {
-    render() {        
+import LoginForm from '../login/loginForm';
+import RegisterForm from './registerForm';
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log("At Login Container", props);
+        this.state = {
+            employees: []
+        }
+    }
+    componentWillMount() {
+        console.log("Loading Component");
+        
+    }
+    componentDidMount() {
+        console.log("Component loaded");
+    }
+
+    getEmpoyeesList() {
+        axios
+        .get(axios.getEmployees())
+        .then((response) => {
+            this.setState({employees: response.data});
+            console.log("At First Response", this.state);
+        })
+        .catch(function (error) {
+            console.log("At First Error", error);
+        });
+    }
+
+    render() {                
         return (
-            <Grid container>
-                Register Page
+            <Grid 
+                className="loginSection" 
+                container 
+                spacing={16} 
+                direction="row"
+                justify="space-between"
+                alignItems="stretch">
+                <Grid item xs={12} sm={6} md={7} lg={7} className="loginSliderContainer">
+                    <div className="slideImageSection">
+                        <img src={Config.images + "kiosk.png"} />
+                    </div>
+                    <div className="loginSliderMessage">
+                        <p>Touchscreen visitor management
+                        designed for senior living.</p>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={5} lg={5} className="loginFormContainer">
+                    <div className="logoSection">
+                        <img src={Config.images + "logo.png"} />
+                    </div>
+                    <div className="loginFormHolder">
+                        <RegisterForm {...this.props} />
+                    </div>
+                </Grid>
             </Grid>
         );
     };
